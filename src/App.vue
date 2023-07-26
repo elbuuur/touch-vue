6d7d8f58d9f7631f89c5f5cdc6c9c1839836f4ff
Ilvina <elbuuur@gmail.com>
2023-07-26 14:53:41 +0300

    add component AddTicker

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <script>
 
 import { loadCoinList, subscribeToTicker, unsubscribeFromTicker } from './api';
+import AddTicker from "@/components/AddTicker.vue";
 
 const LS_KEY = 'cryptonomicon-list'
 
 export default {
   name: "App",
+  components: {AddTicker},
 
   data() {
     return {
       loader: true,
+
       coinList: {},
       hintList: [],
+
       ticker: "",
       tickers: [],
+
       graph: [],
+      maxGraphElements: 1,
+
       selectedTicker: null,
       isError: false,
       filter: '',
@@ -70,6 +77,7 @@ export default {
 
     selectedTicker() {
       this.graph = []
+      this.$nextTick().then(this.calculateMaxGraphElements)
     },
 
     paginatedTickers() {
@@ -91,7 +99,21 @@ export default {
           document.title,
           `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
       );
+    },
+
+    ticker() {
+      this.useHints()
+
+      this.isError = false
     }
+  },
+
+  mounted() {
+    window.addEventListener("resize", this.calculateMaxGraphElements);
+  },
+
+  beforeUnmount() {
+    window.removeEventListener("resize", this.calculateMaxGraphElements);
   },
 
   computed: {
@@ -139,6 +161,14 @@ export default {
   },
 
   methods: {
+    calculateMaxGraphElements() {
+      if (!this.$refs.graph) {
+        return;
+      }
+
+      this.maxGraphElements = this.$refs.graph.clientWidth / 38;
+    },
+
     async getCoinList() {
       try {
         this.coinList = await loadCoinList()
@@ -173,22 +203,33 @@ export default {
       }
     },
 
-    updateTicker(currentTickerName, price) {
-      this.tickers.find(tickersItem => tickersItem.name === currentTickerName).price =
-          price > 1 ? price.toFixed(2) : price.toPrecision(2);
-
-      if (currentTickerName === this.selectedTicker?.name) {
-        this.graph.push(price)
-      }
+    updateTicker(tickerName, price) {
+      this.tickers
+          .filter(t => t.name === tickerName)
+          .forEach(t => {
+            if (t === this.selectedTicker) {
+              this.graph.push(price);
+              while (this.graph.length > this.maxGraphElements) {
+                this.graph.shift();
+              }
+            }
+            t.price = price;
+          });
     },
 
-    addTicker(ticker = this.ticker) {
-      this.filter = ''
-
+    addTicker(ticker) {
       const upperCaseTicker = ticker.toUpperCase()
-      if (!this.validateTicker(upperCaseTicker)) {
+
+      if(!this.validateTicker(upperCaseTicker)) {
+        return;
+      }
+
+      if(this.isError) {
         return
       }
+
+      this.filter = ''
+
 
       let currentTicker = {
         'name': upperCaseTicker,
@@ -224,7 +265,6 @@ export default {
     },
 
     removeTicker(tickerToRemove) {
-      clearInterval(tickerToRemove.idInterval)
 
       this.tickers = this.tickers.filter(item => item !== tickerToRemove)
 
@@ -235,6 +275,10 @@ export default {
     },
 
     selectTicker(ticker) {
+      if(!ticker.isExist) {
+        return
+      }
+
       this.graph = []
       this.selectedTicker = ticker
     },
@@ -259,57 +303,13 @@ export default {
           </svg>
         </div>
         <div class="container">
-          <section>
-            <div class="flex">
-              <div class="max-w-xs">
-                <label for="wallet" class="block text-sm font-medium text-gray-700">Тикер</label>
-                <div class="mt-1 relative rounded-md shadow-md">
-                  <input
-                      v-model="ticker"
-                      @input="useHints"
-                      @keydown.enter.stop="addTicker()"
-                      type="text"
-                      name="wallet"
-                      id="wallet"
-                      class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
-                      placeholder="Например DOGE"
-                  />
-                </div>
-                <div v-if="hintList.length" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
-              <span
-                  v-for="hint in hintList"
-                  @click="addTicker(hint)"
-                  class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-                {{ hint }}
-              </span>
-                </div>
-                <div
-                    v-if="isError"
-                    class="text-sm text-red-600">Такой тикер уже добавлен
-                </div>
-              </div>
-            </div>
-            <button
-                @click.stop="addTicker"
-                type="button"
-                class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            >
-              <!-- Heroicon name: solid/mail -->
-              <svg
-                  class="-ml-0.5 mr-2 h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 24 24"
-                  fill="#ffffff"
-              >
-                <path
-                    d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
-                ></path>
-              </svg>
-              Добавить
-            </button>
-          </section>
+
+          <add-ticker
+              @add-ticker="addTicker"
+              v-model:ticker="ticker"
+              :hint-list="hintList"
+              :is-error="isError"
+          />
 
           <template v-if="tickers.length">
             <hr class="w-full border-t border-gray-600 my-4"/>
@@ -393,8 +393,11 @@ export default {
             <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
               {{ this.selectedTicker?.name }} - USD
             </h3>
-            <div class="flex items-end border-gray-600 border-b border-l h-64">
+            <div
+                ref="graph"
+                class="flex items-end border-gray-600 border-b border-l h-64">
               <div
+                  ref="graphElement"
                   v-for="(bar, idx) in normalizedGraph"
                   :key="idx"
                   :style="{ height: `${bar}%` }"
