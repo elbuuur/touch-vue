50b19fe469515a3debaa64126c1a952899fdcfcc
Ilvina <elbuuur@gmail.com>
2023-07-26 15:46:12 +0300

    add GraphDisplay component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 
 import { loadCoinList, subscribeToTicker, unsubscribeFromTicker } from './api';
 import AddTicker from "@/components/AddTicker.vue";
+import GraphDisplay from "@/components/GraphDisplay.vue";
+
 
 const LS_KEY = 'cryptonomicon-list'
+const ITEMS_PER_PAGE = 6
 
 export default {
   name: "App",
-  components: {AddTicker},
+  components: {GraphDisplay, AddTicker},
 
   data() {
     return {
@@ -77,7 +80,7 @@ export default {
 
     selectedTicker() {
       this.graph = []
-      this.$nextTick().then(this.calculateMaxGraphElements)
+      // this.$nextTick().then(this.calculateMaxGraphElements)
     },
 
     paginatedTickers() {
@@ -108,21 +111,13 @@ export default {
     }
   },
 
-  mounted() {
-    window.addEventListener("resize", this.calculateMaxGraphElements);
-  },
-
-  beforeUnmount() {
-    window.removeEventListener("resize", this.calculateMaxGraphElements);
-  },
-
   computed: {
     startIndex() {
-      return (this.page - 1) * 6
+      return (this.page - 1) * ITEMS_PER_PAGE
     },
 
     endIndex() {
-      return this.page * 6
+      return this.page * ITEMS_PER_PAGE
     },
 
     filteredTickers() {
@@ -161,14 +156,6 @@ export default {
   },
 
   methods: {
-    calculateMaxGraphElements() {
-      if (!this.$refs.graph) {
-        return;
-      }
-
-      this.maxGraphElements = this.$refs.graph.clientWidth / 38;
-    },
-
     async getCoinList() {
       try {
         this.coinList = await loadCoinList()
@@ -265,7 +252,6 @@ export default {
     },
 
     removeTicker(tickerToRemove) {
-
       this.tickers = this.tickers.filter(item => item !== tickerToRemove)
 
       unsubscribeFromTicker(tickerToRemove.name)
@@ -387,51 +373,13 @@ export default {
             </dl>
             <hr class="w-full border-t border-gray-600 my-4"/>
           </template>
-          <section
+
+          <graph-display
               v-if="this.selectedTicker"
-              class="relative">
-            <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-              {{ this.selectedTicker?.name }} - USD
-            </h3>
-            <div
-                ref="graph"
-                class="flex items-end border-gray-600 border-b border-l h-64">
-              <div
-                  ref="graphElement"
-                  v-for="(bar, idx) in normalizedGraph"
-                  :key="idx"
-                  :style="{ height: `${bar}%` }"
-                  class="bg-purple-800 border w-10"
-              ></div>
-            </div>
-            <button
-                @click="selectedTicker = null;"
-                type="button"
-                class="absolute top-0 right-0"
-            >
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
-                  xmlns:svgjs="http://svgjs.com/svgjs"
-                  version="1.1"
-                  width="30"
-                  height="30"
-                  x="0"
-                  y="0"
-                  viewBox="0 0 511.76 511.76"
-                  style="enable-background:new 0 0 512 512"
-                  xml:space="preserve"
-              >
-                <g>
-                  <path
-                      d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                      fill="#718096"
-                      data-original="#000000"
-                  ></path>
-                </g>
-              </svg>
-            </button>
-          </section>
+              :selected-ticker="selectedTicker"
+              :normalized-graph="normalizedGraph"
+              @calculate-graph-elements="maxGraphElements = $event"
+          />
         </div>
       </div>
     </div>
